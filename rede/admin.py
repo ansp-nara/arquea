78abb186836ea84e913cda9e3049c9923ebca0cd
antonio <antonio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2011-11-11 13:48:44 +0000

    Modelos para armazenar a tabela BGP e possibilitar sua análise
    git-svn-id: https://www.ansp.br/svn/sistema@412 9a56ff49-6fb5-480c-b637-73e6884d323a

--- rede/admin.py
+++ rede/admin.py
@@ -3,7 +3,10 @@ from models import *
 
 class BlocoIPAdmin(admin.ModelAdmin):
 
-    list_display = ('entidade', 'AS', '__unicode__', 'obs')
-    search_fields = ('entidade__sigla', 'entidade__nome', 'ip')
+    list_display = ('asn', '__unicode__', 'obs')
+    search_fields = ('asn__entidade__sigla', 'asn__entidade__nome', 'ip')
 
 admin.site.register(BlocoIP, BlocoIPAdmin)
+admin.site.register(Rota)
+admin.site.register(Historico)
+admin.site.register(Provedor)
