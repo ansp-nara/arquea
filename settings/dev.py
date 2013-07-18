434b8411ec52bcaebc8d4cc3d324db7c9a25af99
antonio <antonio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2013-07-18 19:07:19 +0000

    Feito o "split" das configuracoes.
    git-svn-id: https://www.ansp.br/svn/sistema@537 9a56ff49-6fb5-480c-b637-73e6884d323a

---
+++ settings/dev.py
@@ -0,0 +1,19 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/files/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'r(^k*hs*17sjna7i*(fs75=^_r$3(i!r*yok$(n6g61*3s_x3j'
+
+
+CKEDITOR_UPLOAD_PATH='/tmp/ckeditor'
