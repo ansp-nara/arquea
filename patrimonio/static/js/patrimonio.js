9a8792d958168def1780a5ced7a82fb40fdd6a7d
antonio <antonio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2013-09-18 20:59:28 +0000

    Novos templates para patrimonio por numero fmusp
    git-svn-id: https://www.ansp.br/svn/sistema@762 9a56ff49-6fb5-480c-b637-73e6884d323a

--- patrimonio/static/js/patrimonio.js
+++ patrimonio/static/js/patrimonio.js
@@ -1,5 +1,5 @@
 function ajax_numero_fmusp() {
-    if($("#id_tem_numero_fmusp").attr('checked')) {
+    if($("#id_tem_numero_fmusp").is(':checked')) {
         $("#id_numero_fmusp").parent().show();
     }
     else {
