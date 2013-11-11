3fab6a43750ae1541cee2a1aba3c7922d9fd69fe
rogerio <rogerio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2013-11-11 14:47:59 +0000

    jenkins - integracao continua - configuracao de servidor
    git-svn-id: https://www.ansp.br/svn/sistema@867 9a56ff49-6fb5-480c-b637-73e6884d323a

--- settings-test.py
+++ settings-test.py
@@ -18,15 +18,65 @@ INSTALLED_APPS += (
     'django_jenkins',
 )
 
+STATICFILES_DIRS = (
+     '/projetos/workspace/sistema/staticfiles/',
+     '/var/www/files/',
+)
+
+STATIC_ROOT = '/projetos/workspace/sistema/media/'
+STATIC_URL = '/media/'
+MEDIA_URL = '/files/'
+MEDIA_ROOT = '/var/www/files/'
+ADMIN_MEDIA_PREFIX = '/media/'
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '[%(levelname)s] [%(asctime)s] %(name)s.%(funcName)s - %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django': {
+            'handlers': ['null'],
+            'level': 'INFO',
+            'propagate': False
+        },
+    }
+}
+
 
 JENKINS_TASKS = (
     'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.django_tests',   # select one django or
-    #'django_jenkins.tasks.dir_tests'      # directory tests discovery
-#    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-#    'django_jenkins.tasks.run_jslint',
-#    'django_jenkins.tasks.run_csslint',    
-    'django_jenkins.tasks.run_sloccount',    
-#    'django_jenkins.tasks.lettuce_tests',
+ #    'django_jenkins.tasks.dir_tests',      # directory tests discovery
+ #    'django_jenkins.tasks.run_pep8',
+ #    'django_jenkins.tasks.run_pyflakes',
+ #    'django_jenkins.tasks.run_jslint',
+ #    'django_jenkins.tasks.run_csslint',    
+ #    'django_jenkins.tasks.run_sloccount',    
+ #    'django_jenkins.tasks.lettuce_tests',
 )
