d7aa7f0125ae4a7639fce55c64b303b80210aebd
rogerio <rogerio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2013-11-19 21:26:37 +0000

    identificacao - contato - removendo testdocs
    git-svn-id: https://www.ansp.br/svn/sistema@907 9a56ff49-6fb5-480c-b637-73e6884d323a

--- identificacao/tests.py
+++ identificacao/tests.py
@@ -26,6 +26,12 @@ class ContatoTest(TestCase):
         ent1, created = Entidade.objects.get_or_create(sigla='SAC', defaults={'nome': 'Global Crossing', 'cnpj': '00.000.000/0000-00', 'fisco': True, 'url': ''})
         ent2, created = Entidade.objects.get_or_create(sigla='GTECH', defaults={'nome': 'Graneiro Tech', 'cnpj': '00.000.000/0000-00', 'fisco': True, 'url': ''})
         c, created = Contato.objects.get_or_create(primeiro_nome='Joao', ultimo_nome=u"José da Silva Xavier", defaults={'email':'joao@joao.com.br', 'tel': '', 'ativo': True})
+        
+        end1, created = Endereco.objects.get_or_create(entidade=ent1)
+        end2, created = Endereco.objects.get_or_create(entidade=ent2)
+        iden1, created = Identificacao.objects.get_or_create(endereco=end1, contato=c, defaults={'funcao': 'Tecnico', 'area': '', 'ativo': True})
+        iden2, created = Identificacao.objects.get_or_create(endereco=end2, contato=c, defaults={'funcao': 'Gerente', 'area': '', 'ativo': True})
+
 
     def test_unicode(self):
         c = Contato.objects.get(pk=1)
@@ -34,17 +40,17 @@ class ContatoTest(TestCase):
     def test_nome_sem_sobrenome(self):
         c = Contato.objects.get(pk=1)
         c.ultimo_nome = None
-        self.assertEquals(c.nome(), u'Joao')
+        self.assertEquals(c.nome, u'Joao')
 
     def test_sem_nome(self):
         c = Contato.objects.get(pk=1)
         c.primeiro_nome = None
         c.ultimo_nome = None
-        self.assertEquals(c.nome(), u'')
+        self.assertEquals(c.nome, None)
 
     def test_contato_entidade(self):
-        iden = Identificacao.objects.get(pk=1)
-        iden.area = 'TI'
-        self.assertEquals(iden.contato_ent(), u'GTECH, SAC')
+        c = Contato.objects.get(pk=1)
+        c.area = 'TI'
+        self.assertEquals(c.contato_ent(), u'GTECH, SAC')
 
 
