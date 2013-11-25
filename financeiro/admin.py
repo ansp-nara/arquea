da637306904553a0621e8d1b6dd9f28c628ff0af
rogerio <rogerio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2013-11-25 14:36:08 +0000

    Financeiro - auditoria - otimização na exibição do registro de Auditoria para evitar timeout nos testes
    git-svn-id: https://www.ansp.br/svn/sistema@919 9a56ff49-6fb5-480c-b637-73e6884d323a

--- financeiro/admin.py
+++ financeiro/admin.py
@@ -27,6 +27,7 @@ class AuditoriaInline(admin.TabularInline):
     form = AuditoriaAdminForm
     model = Auditoria
     choices = 1
+
   
 class ExtratoCCAdmin(admin.ModelAdmin):
     
@@ -109,6 +110,18 @@ class AuditoriaAdmin(admin.ModelAdmin):
     
     list_display = ('pagamento', 'tipo', 'parcial', 'pagina')
     search_fields = ('parcial', 'pagina')
+    form = AuditoriaAdminForm
+    #list_select_related = True
+    def queryset(self, request):
+        return super(AuditoriaAdmin, self).queryset(request).select_related('estado', 'pagamento', 'tipo', 'pagamento__protocolo', 'pagamento__origem_fapesp__item_outorga__natureza_gasto__modalidade__sigla')
+        
+    def __init__(self, model, admin_site):
+        """
+        Utilizado para setar o admin_site para o forms
+        """
+        self.form.admin_site = admin_site
+        super(AuditoriaAdmin, self).__init__(model, admin_site)
+    
 
 class EstadoAdmin(admin.ModelAdmin):
     search_fields = ('nome',)
