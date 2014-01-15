bfac1d168868dd6a6e7a0d44c336851ddc9cd27e
rogerio <rogerio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2014-01-15 14:29:03 +0000

    django 1.6 - alterações para deploy
    git-svn-id: https://www.ansp.br/svn/sistema@993 9a56ff49-6fb5-480c-b637-73e6884d323a

---
+++ sistema/settings-dev.py
@@ -0,0 +1,125 @@
+from settings import *
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/files/'
+MEDIA_ROOT = '/var/www/files/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'blablabla'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    'C:/projetos/workspace/sistema/templates',
+)
+
+CKEDITOR_UPLOAD_PATH='/tmp/ckeditor'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '127.0.0.1',
+        'NAME': 'sistema-bak-20130927',
+#         'NAME': 'sistema',
+        'USER': 'sistema-user',
+        'PASSWORD': '1234'
+        
+#         'HOST': '10.0.0.97',
+#         'NAME': 'rogerio',
+#         'USER': 'rogerio',
+#         'PASSWORD': 'sistema'
+
+    }
+}
+          
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='nara@ansp.br'
+EMAIL_HOST_PASSWORD='1234'
+EMAIL_USE_TLS=True
+
+STATICFILES_DIRS = (
+     '/projetos/workspace/sistema/staticfiles/',
+     '/var/www/files/',
+)
+
+STATIC_ROOT = '/projetos/workspace/sistema/media/'
+STATIC_URL = '/media/'
+
+
+INSTALLED_APPS += (
+    'django_jenkins',
+)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '[%(levelname)s] [%(asctime)s] %(name)s.%(funcName)s - %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django': {
+            'handlers': ['null'],
+            'level': 'INFO',
+            'propagate': False
+        },
+    }
+}
+
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.with_coverage',
+    'django_jenkins.tasks.django_tests',   # select one django or
+    #'django_jenkins.tasks.dir_tests'      # directory tests discovery
+#    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+#    'django_jenkins.tasks.run_jslint',
+#    'django_jenkins.tasks.run_csslint',    
+    'django_jenkins.tasks.run_sloccount',    
+#    'django_jenkins.tasks.lettuce_tests',
+)
+
+
+WKHTMLTOPDF_CMD = 'C:/Program Files (x86)/wkhtmltopdf/wkhtmltopdf.exe'
+WKHTMLTOPDF_CMD_OPTIONS = {
+    'quiet': True,
+    # Para o Linux, descomentar esta linha
+    # 'zoom':0.63,
+}
--- urls.py
+++ urls.py
@@ -13,7 +13,7 @@ admin.site.root_path = "/admin/" # there is probably a bug in django...
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^sistema/', include('sistema.foo.urls')),
+    # (r'^sistema/', include('foo.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
@@ -22,26 +22,35 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^files/(?P<filename>.*)', 'utils.views.serve_files'),
-    (r'^protocolo/', include('sistema.protocolo.urls')),
-    (r'^patrimonio/', include('sistema.patrimonio.urls')),
-    (r'^financeiro/', include('sistema.financeiro.urls')),
-    (r'^outorga/', include('sistema.outorga.urls')),
-    (r'^memorando/', include('sistema.memorando.urls')),
-    (r'^monitor/', include('sistema.monitor.urls')),
-    (r'^identificacao/', include('sistema.identificacao.urls')),
-    (r'^membro/', include('sistema.membro.urls')),
-    (r'^rede/', include('sistema.rede.urls')),
-    (r'^processo/', include('sistema.processo.urls')),
+    (r'^protocolo/', include('protocolo.urls')),
+    (r'^patrimonio/', include('patrimonio.urls')),
+    (r'^financeiro/', include('financeiro.urls')),
+    (r'^outorga/', include('outorga.urls')),
+    (r'^memorando/', include('memorando.urls')),
+    (r'^monitor/', include('monitor.urls')),
+    (r'^identificacao/', include('identificacao.urls')),
+    (r'^membro/', include('membro.urls')),
+    (r'^rede/', include('rede.urls')),
+    (r'^processo/', include('processo.urls')),
     (r'^verificacao/', include('verificacao.urls')),
     (r'^carga/', include('carga.urls')),
     (r'^accounts/login/$', 'django_cas.views.login'),
     (r'^accounts/logout/$', 'django_cas.views.logout'),
-    (r'^tinymce/', include('tinymce.urls')),
     (r'^xml_rpc_srv$', 'xmlrpc_srv.rpc_handler', {'SSL':False}),
     (r'^verifica$', 'utils.views.verifica', {'SSL':False}),
     (r'^sempermissao$', TemplateView.as_view(template_name="401.html")),
-    (r'^ckeditor/', include('ckeditor.urls')),
     (r'^logs$', 'utils.views.uso_admin'),
+
+    # necessário instalar o django_localflavor
+    # No Django 1.6 o django.contrib.localflavor foi desabilitado
+    
+    # necessário instalar o django_wkhtmltopdf para conversão de PDF
+
+    # necessário instalar/atualizar o django_tinymce
+    (r'^tinymce/', include('tinymce.urls')),
+    # necessário instalar o django_ckeditor_updated (para o Django 1.6)
+    (r'^ckeditor/', include('ckeditor.urls')),
+
     
     (r'^', include(admin.site.urls)),
 )# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
