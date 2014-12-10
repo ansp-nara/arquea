91b9e4e6b50278ceb18dbdea55f6cf7cdf35a613
antonio <antonio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2014-12-10 21:36:36 +0000

    Inclusão do campo 'cartao' no extrato da conta corrente indicando se a origem da entrada no extrato foi a conta corrente ou o cartão.
    git-svn-id: https://www.ansp.br/svn/sistema/src/trunk/sistema@1494 9a56ff49-6fb5-480c-b637-73e6884d323a

--- financeiro/management/commands/carregaextrato.py
+++ financeiro/management/commands/carregaextrato.py
@@ -7,28 +7,39 @@ from financeiro.models import ExtratoCC
 
 
 class Command(BaseCommand):
-    args = '<arquivo_extrato>'
+    args = u'<arquivo_extrato cc/ct >'
     help = 'Carrega um extrato de conta corrente'
     
     def handle(self, *args, **options):
-        if len(args) < 1:
+        if len(args) < 2:
             self.stdout.write('Nome de arquivo faltando')
             return
         
         arq = open(args[0])
         for l in arq:
             dados = l.split()
-            data = dados.pop(0)
-            sinal = dados.pop()
-            valor = dados.pop()
-            codigo = dados.pop()
-            historico = ' '.join(dados)
+            if args[1] == 'cc':
+                data = dados.pop(0)
+                sinal = dados.pop()
+                valor = dados.pop()
+                codigo = dados.pop()
+                historico = ' '.join(dados)
+                cartao = False
+            else:
+                codigo = dados[0]
+                data = dados[1]
+                valor = dados[2]
+                sinal = dados[3]
+                historico = ' '.join(dados[5:-3])
+                cartao = True
+
             (d, m, y) = map(int, data.split('/'))
+            if y < 2000: y += 2000
             data = date(y,m,d)
             valor = re.sub('\.', '', valor)
             valor = re.sub(',', '.', valor)
             codigo = re.sub('\.', '', codigo)
             sinal = '' if sinal=='C' else '-'
-            ex = ExtratoCC.objects.create(data_oper=data, cod_oper=codigo, valor=Decimal('%s%s' % (sinal, valor)), historico=historico, despesa_caixa=False)
+            ex = ExtratoCC.objects.create(data_oper=data, cod_oper=codigo, valor=Decimal('%s%s' % (sinal, valor)), historico=historico, despesa_caixa=False, cartao=cartao)
             
         self.stdout.write('Extrato inserido')
