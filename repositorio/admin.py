39c6d2c858870822ce3d16f314f1eb69473e0e39
rogerio <rogerio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2015-07-01 20:07:25 +0000

    repositorio - Repositorio - filtro por tipo, natureza e estado na lista de registros
    git-svn-id: https://www.ansp.br/svn/sistema/src/trunk/sistema@1666 9a56ff49-6fb5-480c-b637-73e6884d323a

--- repositorio/admin.py
+++ repositorio/admin.py
@@ -2,33 +2,40 @@
 from django.contrib import admin
 from repositorio.models import Repositorio, Ticket, Tipo, Estado, Natureza, Servico, Anexo
 from repositorio.forms import RepositorioAdminForm
+from utils.admin import RelatedOnlyFieldListFilter
 
 class TicketInline(admin.TabularInline):
-	model = Ticket
-	extra = 1
-	
+    model = Ticket
+    extra = 1
+    
 class AnexoInline(admin.TabularInline):
-	model = Anexo
-	extra = 1
-	
+    model = Anexo
+    extra = 1
+    
+
 class RepositorioAdmin(admin.ModelAdmin):
-	"""
-	Interface administrativa para o repositório
-	"""
-	inlines = [TicketInline, AnexoInline]
-	form = RepositorioAdminForm
-	
-	fieldsets = (
-		(None, {
-			'fields': ('data_ocorrencia', ('tipo', 'natureza', 'estado'), 'servicos', 'ocorrencia', 'anterior', 'memorandos', ('filtra_patrimonio', 'patrimonios'), ('responsavel', 'demais'), 'obs'),
-			'classes': 'wide',
-		}),
-	)
-	
-	readonly_fields = ('num_rep',)
-	list_display = ('num_rep', 'data', 'data_ocorrencia', 'tipo', 'servicos_display', 'natureza', 'estado')
-	search_fields = ('ocorrencia', 'tipo__nome', 'natureza__nome', 'servicos__nome')
-	
+    """
+    Interface administrativa para o repositório
+    """
+    inlines = [TicketInline, AnexoInline]
+    form = RepositorioAdminForm
+    
+    fieldsets = (
+        (None, {
+            'fields': ('data_ocorrencia', ('tipo', 'natureza', 'estado'), 'servicos', 'ocorrencia', 'anterior', 'memorandos', ('filtra_patrimonio', 'patrimonios'), ('responsavel', 'demais'), 'obs'),
+            'classes': 'wide',
+        }),
+    )
+    
+    readonly_fields = ('num_rep',)
+    list_display = ('num_rep', 'data', 'data_ocorrencia', 'tipo', 'servicos_display', 'natureza', 'estado')
+    search_fields = ('ocorrencia', 'tipo__nome', 'natureza__nome', 'servicos__nome')
+    list_filter = (('tipo', RelatedOnlyFieldListFilter), 
+                   ('natureza', RelatedOnlyFieldListFilter),
+                   ('estado', RelatedOnlyFieldListFilter),
+                  )
+
+
 # Register your models here.
 admin.site.register(Repositorio, RepositorioAdmin)
 admin.site.register(Tipo)
