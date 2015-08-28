252d91b2deb48c7c3595d05ea4bd22952ea144f0
rogerio <rogerio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2015-08-28 18:22:34 +0000

    patrimonio - bug: export para excel não estava mostrando dados nas colunas
    git-svn-id: https://www.ansp.br/svn/sistema/src/trunk/sistema@1706 9a56ff49-6fb5-480c-b637-73e6884d323a

--- patrimonio/modelsResource.py
+++ patrimonio/modelsResource.py
@@ -10,14 +10,14 @@ Definição de ModelResource utilizados no import_export para fazer a exportaç�
 """
 class PatrimonioResource(resources.ModelResource):
     pagamento__protocolo__termo = fields.Field(column_name='termo')
-    equipamento__part_number = fields.Field(column_name='part number')
-    equipamento__modelo = fields.Field(column_name='modelo')
-    equipamento__entidade_fabricante__sigla = fields.Field(column_name='fabricante')
-    ns = fields.Field(column_name='serial number')
-    tipo__nome = fields.Field(column_name='tipo')
-    numero_fmusp = fields.Field(column_name='num fmusp')
-    entidade_procedencia__sigla = fields.Field(column_name='procedencia')
-    garantia_termino = fields.Field(column_name='termino de garantia')
+    equipamento__part_number = fields.Field(column_name='part number', attribute='equipamento__part_number')
+    equipamento__modelo = fields.Field(column_name='modelo', attribute='equipamento__modelo')
+    equipamento__entidade_fabricante__sigla = fields.Field(column_name='fabricante', attribute='equipamento__entidade_fabricante__sigla')
+    ns = fields.Field(column_name='serial number', attribute='ns')
+    tipo__nome = fields.Field(column_name='tipo', attribute='tipo__nome')
+    numero_fmusp = fields.Field(column_name='num fmusp', attribute='numero_fmusp')
+    entidade_procedencia__sigla = fields.Field(column_name='procedencia', attribute='entidade_procedencia__sigla')
+    garantia_termino = fields.Field(column_name='termino de garantia', attribute='garantia_termino')
                   
     class Meta:
         model = Patrimonio
@@ -67,16 +67,15 @@ class PatrimonioResource(resources.ModelResource):
         return ''
 
 
-
 class RelatorioPorTipoResource(resources.ModelResource):
-    checado = fields.Field(column_name='Checado')
-    id = fields.Field(column_name='ID')
-    procedencia = fields.Field(column_name='Procedência')
-    fabricante = fields.Field(column_name='Marca')
-    modelo = fields.Field(column_name='Modelo')
-    part_number = fields.Field(column_name='Part number')
-    descricao = fields.Field(column_name='Descrição')
-    ns = fields.Field(column_name='NS')
+    checado = fields.Field(column_name='Checado', attribute='checado')
+    id = fields.Field(column_name='ID', attribute='id')
+    procedencia = fields.Field(column_name='Procedência', attribute='entidade_procedencia')
+    fabricante = fields.Field(column_name='Marca', attribute='equipamento__entidade_fabricante__sigla')
+    modelo = fields.Field(column_name='Modelo', attribute='equipamento__modelo')
+    part_number = fields.Field(column_name='Part number', attribute='equipamento__part_number')
+    descricao = fields.Field(column_name='Descrição', attribute='descricao')
+    ns = fields.Field(column_name='NS', attribute='ns')
     local = fields.Field(column_name='Local')
     posicao = fields.Field(column_name='Posição')
     estado = fields.Field(column_name='Estado')
@@ -112,18 +111,6 @@ class RelatorioPorTipoResource(resources.ModelResource):
                   'nf',
                  )
 
-    def dehydrate_checado(self, p): return '%s' % (p.checado or '')
-    def dehydrate_id(self, p): return '%s' % (p.id or '')
-    def dehydrate_procedencia(self, p): return '%s' % (p.procedencia or '')
-    def dehydrate_fabricante(self, p): 
-        try:
-            return '%s' % (p.equipamento.entidade_fabricante.sigla or '')
-        except AttributeError:
-            return '' 
-    def dehydrate_modelo(self, p): return '%s' % (p.modelo or '')
-    def dehydrate_part_number(self, p): return '%s' % (p.part_number or '')
-    def dehydrate_descricao(self, p): return '%s' % (p.descricao or '')
-    def dehydrate_ns(self, p): return '%s' % (p.ns or '')
     def dehydrate_local(self, p):
         try:
             return '%s' % (p.historico_atual.endereco.end or '')
