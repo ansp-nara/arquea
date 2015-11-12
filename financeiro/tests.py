350f0a0da573c6df0abfc789d450cebe1c7140a6
rogerio <rogerio@9a56ff49-6fb5-480c-b637-73e6884d323a>
2015-11-12 21:09:24 +0000

    financeiro - testes - ajustando a carga de Modalidade para não dar conflito com os fixtures
    git-svn-id: https://www.ansp.br/svn/sistema/src/trunk/sistema@1798 9a56ff49-6fb5-480c-b637-73e6884d323a

--- financeiro/tests.py
+++ financeiro/tests.py
@@ -26,8 +26,8 @@ class ExtratoCCTest(TestCase):
                                     termino=date(2008, 12, 31), data_presta_contas=date(2008, 2, 28))
 
         # Cria Natureza de gasto
-        m1 = Modalidade.objects.create(sigla='STB', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
-        m11 = Modalidade.objects.create(sigla='STB1', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
+        m1 = Modalidade.objects.create(sigla='STB1', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
+        m11 = Modalidade.objects.create(sigla='STB2', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
         m2 = Modalidade.objects.create(sigla='STE', nome='Servicos de Terceiro no Exterior', moeda_nacional=False)
 
         n1 = Natureza_gasto.objects.create(modalidade=m1, termo=t, valor_concedido='1500000.00')
@@ -38,7 +38,7 @@ class ExtratoCCTest(TestCase):
                                        fisco=True, url='')
         ent2 = Entidade.objects.create(sigla='SAC', nome='SAC do Brasil', cnpj='00.000.000/0000-00',
                                        fisco=True, url='')
-        
+
         end1 = Endereco.objects.create(entidade=ent1)
         end2 = Endereco.objects.create(entidade=ent2)
 
@@ -86,7 +86,7 @@ class ExtratoCCTest(TestCase):
 
         fp1 = Pagamento.objects.create(protocolo=p1, conta_corrente=ex1, origem_fapesp=of1, valor_fapesp='2650')
         fp2 = Pagamento.objects.create(protocolo=p2, conta_corrente=ex2, origem_fapesp=of2, valor_fapesp='250000')
-        
+
         efi1 = EstadoFinanceiro.objects.create(nome="Estado financeiro 1")
         tcomprov1 = TipoComprovante.objects.create(nome="Tipo Comprovante 1")
 
@@ -96,18 +96,18 @@ class ExtratoCCTest(TestCase):
     def test_unicode(self):
         extrato = ExtratoCC.objects.get(pk=1)
         self.assertEquals(extrato.__unicode__(), u'2008-10-05 - 333333 - TED - 2650.00')
-        
+
     def test_saldo(self):
         extrato = ExtratoCC.objects.get(pk=1)
         self.assertEquals(extrato.saldo, Decimal('252650.00'))
-        
+
     def test_saldo_anterior(self):
         extrato = ExtratoCC.objects.get(pk=1)
         self.assertEquals(extrato.saldo_anterior, Decimal('250000.00'))
-        
+
     def test_parciais(self):
         extrato = ExtratoCC.objects.get(pk=1)
-        self.assertEquals(extrato.parciais(), u'STB [parcial 101 (102)]  ')
+        self.assertEquals(extrato.parciais(), u'STB1 [parcial 101 (102)]  ')
 
 
 class TipoComprovanteFinanceiroTest(TestCase):
@@ -117,7 +117,7 @@ class TipoComprovanteFinanceiroTest(TestCase):
     def test_unicode(self):
         tipo = TipoComprovante.objects.get(pk=1)
         self.assertEquals(tipo.__unicode__(), u'Tipo Comprovante 1')
-        
+
 
 class ExtratoFinanceiroTest(TestCase):
     def setUp(self):
@@ -126,23 +126,23 @@ class ExtratoFinanceiroTest(TestCase):
         t = Termo.objects.create(ano=2008, processo=22222, digito=2, inicio=date(2008, 1, 1), estado=e)
 
         tcomprov1 = TipoComprovanteFinanceiro.objects.create(nome="Tipo Comprovante Financeiro 1")
-        
+
         exf1 = ExtratoFinanceiro.objects.create(termo=t, data_libera='2013-08-10', cod='EFC', historico="historico",
                                                 valor=123456, tipo_comprovante=tcomprov1, parcial=543)
 
     def test_unicode(self):
         exf = ExtratoFinanceiro.objects.get(pk=1)
         self.assertEquals(exf.__unicode__(), u'2013-08-10 - EFC - historico - 123456.00')
-        
+
     def test_despesa_caixa_falso(self):
         exf = ExtratoFinanceiro.objects.get(pk=1)
         self.assertFalse(exf.despesa_caixa)
-        
+
     def test_despesa_caixa(self):
         tcomprov1 = TipoComprovanteFinanceiro(pk=1)
         tcomprov1.nome = u'Despesa de caixa' 
         tcomprov1.save()
-        
+
         exf = ExtratoFinanceiro.objects.get(pk=1)
         exf.tipo=tcomprov1
         exf.save()
@@ -165,8 +165,8 @@ class PagamentoTest(TestCase):
         o2 = Outorga.objects.create(termo=t, categoria=c2, data_solicitacao=date(2008, 4, 1),
                                     termino=date(2008, 12, 31), data_presta_contas=date(2008, 2, 28))
         # Cria Natureza de gasto
-        m1 = Modalidade.objects.create(sigla='STB', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
-        m11 = Modalidade.objects.create(sigla='STB1', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
+        m1 = Modalidade.objects.create(sigla='STB1', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
+        m11 = Modalidade.objects.create(sigla='STB2', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
         m2 = Modalidade.objects.create(sigla='STE', nome='Servicos de Terceiro no Exterior', moeda_nacional=False)
 
         n1 = Natureza_gasto.objects.create(modalidade=m1, termo=t, valor_concedido='1500000.00')
@@ -177,7 +177,7 @@ class PagamentoTest(TestCase):
                                        url='')
         ent2 = Entidade.objects.create(sigla='SAC', nome='SAC do Brasil', cnpj='00.000.000/0000-00', fisco=True,
                                        url='')
-        
+
         end1 = Endereco.objects.create(entidade=ent1)
         end2 = Endereco.objects.create(entidade=ent2)
 
@@ -229,13 +229,13 @@ class PagamentoTest(TestCase):
 
     def test_unicode(self):
         exf = Pagamento.objects.get(pk=1)
-        self.assertEquals(exf.__unicode__(), u'8888 - 2650.00 - STB    ID: 1')
-    
+        self.assertEquals(exf.__unicode__(), u'8888 - 2650.00 - STB1    ID: 1')
+
     def test_unicode_com_patrocinio(self):
         exf = Pagamento.objects.get(pk=1)
         exf.valor_patrocinio = 1234
-        
-        self.assertEquals(exf.__unicode__(), u'8888 - 3884.00 - STB    ID: 1')
+
+        self.assertEquals(exf.__unicode__(), u'8888 - 3884.00 - STB1    ID: 1')
 
     def test_unicode_com_auditoria(self):
         exf = Pagamento.objects.get(pk=1)
@@ -245,8 +245,8 @@ class PagamentoTest(TestCase):
 
         audit1 = Auditoria.objects.create(estado=efi1, pagamento=exf, tipo=tcomprov1, parcial=101.0, pagina=102.0,
                                           obs='observacao')
-        
-        self.assertEquals(exf.__unicode__(), u'8888 - 2650.00 - STB, parcial 101, página 102    ID: 1')
+
+        self.assertEquals(exf.__unicode__(), u'8888 - 2650.00 - STB1, parcial 101, página 102    ID: 1')
 
 
     def test_unicode_para_auditoria(self):
@@ -257,8 +257,8 @@ class PagamentoTest(TestCase):
 
         audit1 = Auditoria.objects.create(estado=efi1, pagamento=exf, tipo=tcomprov1, parcial=101.0, pagina=102.0,
                                           obs='observacao')
-        
-        self.assertEquals(exf.unicode_para_auditoria(), u'8888 - 2650.00 - STB    ID: 1')
+
+        self.assertEquals(exf.unicode_para_auditoria(), u'8888 - 2650.00 - STB1    ID: 1')
 
     def test_codigo_operacao(self):
         exf = Pagamento.objects.get(pk=1)
@@ -267,13 +267,13 @@ class PagamentoTest(TestCase):
     def test_codigo_operacao_vazio(self):
         exf = Pagamento.objects.get(pk=1)
         exf.conta_corrente = None
-        
+
         self.assertEquals(exf.codigo_operacao(), '')
 
     def test_anexos(self):
         exf = Pagamento.objects.get(pk=1)
         exf.conta_corrente = None
-        
+
         self.assertEquals(exf.anexos(), u'Não')
 
     def test_anexos_com_auditoria(self):
@@ -290,44 +290,44 @@ class PagamentoTest(TestCase):
 
     def test_termo(self):
         exf = Pagamento.objects.get(pk=1)
-        
+
         self.assertEquals(exf.termo(), u'08/22222-2')
 
     def test_item(self):
         exf = Pagamento.objects.get(pk=1)
-        
+
         self.assertEquals(exf.item(), u'08/22222-2 - Armazenagem')
 
     def test_item_sem_origem_fapesp(self):
         exf = Pagamento.objects.get(pk=1)
         exf.origem_fapesp = None
-        
+
         self.assertEquals(exf.item(), u'Não é Fapesp')
 
     def test_data(self):
         exf = Pagamento.objects.get(pk=1)
-        
+
         self.assertEquals(exf.data(), date(2008,10,5))
 
     def test_nota(self):
         exf = Pagamento.objects.get(pk=1)
-        
+
         self.assertEquals(exf.nota(), u'8888')
 
     def test_formata_valor_fapesp(self):
         exf = Pagamento.objects.get(pk=1)
-        
+
         self.assertEquals(exf.formata_valor_fapesp(), u'R$ 2.650,00')
 
     def test_formata_valor_fapesp_dolar(self):
         exf = Pagamento.objects.get(pk=1)
         exf.origem_fapesp.item_outorga.natureza_gasto.modalidade.moeda_nacional = False
-        
+
         self.assertEquals(exf.formata_valor_fapesp(), u'US$ 2.650,00')
 
     def test_pagina(self):
         exf = Pagamento.objects.get(pk=1)
-        
+
         self.assertEquals(exf.pagina(), u'')
 
     def test_pagina_com_auditoria(self):
@@ -344,7 +344,7 @@ class PagamentoTest(TestCase):
 
     def test_parcial(self):
         exf = Pagamento.objects.get(pk=1)
-         
+
         self.assertEquals(exf.parcial(), u'')
 
     def test_parcial_com_auditoria(self):
@@ -403,8 +403,8 @@ class AuditoriaTest(TestCase):
                                     termino=date(2008, 12, 31), data_presta_contas=date(2008, 2, 28))
 
         #Cria Natureza de gasto
-        m1 = Modalidade.objects.create(sigla='STB', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
-        m11 = Modalidade.objects.create(sigla='STB1', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
+        m1 = Modalidade.objects.create(sigla='STB1', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
+        m11 = Modalidade.objects.create(sigla='STB2', nome='Servicos de Terceiro no Brasil', moeda_nacional=True)
         m2 = Modalidade.objects.create(sigla='STE', nome='Servicos de Terceiro no Exterior', moeda_nacional=False)
 
         n1 = Natureza_gasto.objects.create(modalidade=m1, termo=t, valor_concedido='1500000.00')
@@ -415,7 +415,7 @@ class AuditoriaTest(TestCase):
                                        url='')
         ent2 = Entidade.objects.create(sigla='SAC', nome='SAC do Brasil', cnpj='00.000.000/0000-00', fisco=True,
                                        url='')
-        
+
         end1 = Endereco.objects.create(entidade=ent1)
         end2 = Endereco.objects.create(entidade=ent2)
 
@@ -462,7 +462,7 @@ class AuditoriaTest(TestCase):
 
         fp1 = Pagamento.objects.create(protocolo=p1, conta_corrente=ex1, origem_fapesp=of1, valor_fapesp='2650')
         fp2 = Pagamento.objects.create(protocolo=p2, conta_corrente=ex2, origem_fapesp=of2, valor_fapesp='250000')
-        
+
         efi1 = EstadoFinanceiro.objects.create(nome="Estado financeiro 1")
         tcomprov1 = TipoComprovante.objects.create(nome="Tipo Comprovante 1")
 
