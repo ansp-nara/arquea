6fbe4cb149ac22afe68835e267115946e0193b1e
antonio <ajoaoff@gmail.com>
2015-12-07 11:26:10 -0200

    Atualizado para utilizar utils ao invés de util.
    Django 1.9 eliminou as instâncias de util.py, substituindo
    por utils.py

--- rede/forms.py
+++ rede/forms.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.db.models import Q
-from django.forms.util import ErrorList
+from django.forms.utils import ErrorList
 from django.utils.html import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
--- repositorio/forms.py
+++ repositorio/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.forms.util import ErrorList
+from django.forms.utils import ErrorList
 from django.db.models import Q
 from repositorio.models import Repositorio, Tipo
 from patrimonio.models import Patrimonio
