15ceee0b8ff75daa136956565e1e34547e634c9d
antonio <ajoaoff@gmail.com>
2015-12-08 16:40:13 -0200

    Cabeçalho da licença MPL acrescentado aos arquivos javascript

--- configuracao/static/js/por_tipo.js
+++ configuracao/static/js/por_tipo.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 function sel_tipo_equip() {
     tipo = document.getElementById('id_tipo')
     estado = document.getElementById('id_estado')
